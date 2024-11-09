ca704dff665fc1d8e9df1ba44e1435d31aafb4d3
manuehhhhh <mamartinez.23@est.ucab.edu.ve>
2024-11-09 11:43:37 -0400

    inicio del backend por spring

---
+++ albergo/albergo/src/main/java/ingenieria/de/software/_0/albergo/AlbergoApplication.java
@@ -0,0 +1,13 @@
+package ingenieria.de.software._0.albergo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbergoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbergoApplication.class, args);
+	}
+
+}
---
+++ albergo/albergo/src/test/java/ingenieria/de/software/_0/albergo/AlbergoApplicationTests.java
@@ -0,0 +1,13 @@
+package ingenieria.de.software._0.albergo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbergoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
